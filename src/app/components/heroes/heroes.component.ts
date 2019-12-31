522de98cbe6a2a433e156aca29cf2b70c9497ef0
nikolas2603843 <55547972+nikolas2603843@users.noreply.github.com>
2019-12-31 09:55:46 -0500

    Update heroes.component.ts

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroresService, Heroe } from '../../servicios/herores.service';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
 })
 export class HeroesComponent implements OnInit {
   heroes:Heroe[] = [];
-  constructor(private _heroresService:HeroresService) { }
+  constructor(private _heroresService:HeroresService
+  private _router:Router){
+  }
 
   ngOnInit() {
     this.heroes=this._heroresService.getHeroes();
     console.log(this.heroes);
       
+  }
+   verHeroe(idx:number){
+this._router.navigate(['/heroe',idx]);
   }
 
 
